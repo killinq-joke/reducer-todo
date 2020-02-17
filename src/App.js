9a9c7367ccabd755bcc2f97c6c0bdb2ee7336bcf
Zak <zakadmaster@gmail.com>
2020-02-17 14:35:37 +0100

    Rearrangement of files

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
 import React, { useReducer } from "react";
+import reducer, { initialState, INPUT_CHANGE, ADD_TODO, MARK_COMPLETED, CLEAR_COMPLETED } from './reducers'
 import uuid from "uuid";
 
-const initialState = {
-  todos: [],
-  newTodoValue: ""
-};
 
-const INPUT_CHANGE = "INPUT_CHANGE";
-const ADD_TODO = "ADD_TODO";
-const MARK_COMPLETED = "MARK_COMPLETED";
-const CLEAR_COMPLETED = "CLEAR_COMPLETED";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case INPUT_CHANGE:
-      console.log(action.payload);
-      return {
-        ...state,
-        newTodoValue: action.payload
-      };
-    case ADD_TODO:
-      const newTodo = action.payload;
-      return {
-        ...state,
-        todos: state.todos.concat(newTodo)
-      };
-    case MARK_COMPLETED:
-      const id = action.payload;
-      return {
-        ...state,
-        todos: state.todos.map(todo => {
-          if(todo.id === id) {
-            
-            return {
-              ...todo,
-            completed: !todo.completed
-            } 
-          }
-          return todo;
-        })
-      }  
-    case CLEAR_COMPLETED:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.completed === false)
-      }  
-    default:
-      // if the action.type does not match any of the cases in the switch,
-      // we still need to return app state
-      return state;
-  }
-}
+
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
