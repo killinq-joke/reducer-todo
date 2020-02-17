9129112fa2a96bd79f7cf3d19fd528a30e4f9299
Zak <zakadmaster@gmail.com>
2020-02-17 13:39:13 +0100

    Addition of initial state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useReducer } from "react";
 import uuid from 'uuid'
 
+const initialState = {
+  todos: [],
+  newTodoValue: ""
+}
+
+const INPUT_CHANGE = 'INPUT_CHANGE'
+const ADD_TODO = 'ADD_TODO'
+const MARK_COMPLETED = 'MARK_COMPLETED'
+const CLEAR_COMPLETED = 'CLEAR_COMPLETED'
+
 export default function App() {
 
   return(
     <div>
-
+      <label></label>
+      <input></input>
     </div>
   )
 }
